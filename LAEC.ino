bb6b82fec55e8e8918a7e2256df788dbc823622c
Emily Callejas Torrico <emilycallejastorrico@gmail.com>
2020-08-17 17:37:14 -0400

    Paso 1 del código

---
+++ LAEC.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2, 1);
+int PIR;
+
+void setup (){
+    lcd.being(16, 2);
+    lcd.print("¡Todo esta bien!");
+    PIR = 0;
+    pinMode(7, INPUT);
+}
+
+VOID LOOP(){
+    if(digitalRead(7)){
+        lcd.setCursor(0,0);
+        lcd.print("Detecto algo!!!!");
+        lcd.setCursor(0,1);
+        lcd.print(millis() / 1000);
+        delay (500);
+    }
+}
